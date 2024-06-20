9b8f0bb590b0b8d30c715da00cd8319119faa937
Basuru89 <148215082+Basuru89@users.noreply.github.com>
2024-06-20 13:41:12 +0000

    Added secondary windows.

--- app.py
+++ app.py
@@ -1,12 +1,55 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder= 'Templates')
 
 @app.route('/')
-def hellow_world():
-  return render_template('home.html')
+def home():
+      return render_template('home.html')
 
+@app.route('/blog')
+
+def blog():
+      return render_template('blog.html')
+
+@app.route('/community')
+
+def community():
+      return render_template('community.html')
+
+@app.route('/contact')
+
+def contact():
+      return render_template('contact.html')
+
+@app.route('/faqs')
+
+def faqs():
+      return render_template('faqs.html')
+
+@app.route('/feedback')
+
+def feedback():
+      return render_template('feedback.html')
+
+@app.route('/join')
+
+def join():
+      return render_template('join.html')
+
+@app.route('/products')
+
+def products():
+      return render_template('products.html')
+
+@app.route('/reviews')
+
+def reviews():
+      return render_template('reviews.html')
+
+@app.route('/sustainability')
+
+def sustainability():
+      return render_template('sustainability.html')
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+      app.run(host="0.0.0.0", debug=True)
